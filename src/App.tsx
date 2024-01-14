14f18d003675a252da468e0447b27e8c6e4597c3
hasanisu <hasanjimam@gmail.com>
2024-01-14 17:57:08 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Container from "./components/ui/Container";
+
+function App() {
+  return (
+    <Container>
+      <h1>Main Content</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="h-16 w-full bg-purple-400">
+      <h1>Navbar Layout</h1>
+    </div>
+  );
+};
+
+export default Navbar;
